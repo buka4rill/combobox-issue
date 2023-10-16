b4b334c37282a64050cfb90dd0fff999378ed9ac
Ebuka Abraham <45390219+buka4rill@users.noreply.github.com>
2023-10-16 16:30:32 +0200

    bug report

--- src/App.tsx
+++ src/App.tsx
@@ -1,85 +1,46 @@
-import useLocalStorageState from 'use-local-storage-state';
-import {
-  Button,
-  CenteredColumn,
-  Card,
-  Heading,
-  Text,
-  Logo,
-  Input,
-  Divider,
-  marginX,
-  marginY,
-  PasswordInput,
-  Link,
-  getBackgroundColor,
-  gap,
-  darkTheme,
-  Toggle,
-  top,
-  end,
-  lightTheme,
-  paddingY,
-} from '@coachhubio/nova';
-import styled, { ThemeProvider } from 'styled-components';
+import { Combobox, Option } from '@coachhubio/nova';
 
-const Wrapper = styled(CenteredColumn)`
-  justify-content: center;
-  min-height: 100%;
-  flex-shrink: 0;
-
-  ${getBackgroundColor('background-color-subdued')}
-  ${gap('3xl')}
-  ${paddingY('3xl')}
-`;
-
-const LoginFormCard = styled(Card)`
-  width: 480px;
-  max-width: 90%;
-  flex-direction: column;
-
-  ${Divider} {
-    ${marginX('l', { negative: true })}
-    ${marginY('l')}
-  }
-`;
-
-const ThemeSwitcher = styled(Toggle)`
-  position: absolute;
-  ${top('l')}
-  ${end('l')}
-`;
+import { useEffect, useState } from 'react';
 
 function App() {
-  const [dark, setDark] = useLocalStorageState('dark', { defaultValue: false });
+  // mock api call
+  // Imagine this is from an api call
+  const org = [
+    {
+      id: '1',
+      name: 'Org 1',
+    },
+    {
+      id: '2',
+      name: 'Org 2',
+    },
+  ];
 
+  const [label, setLabel] = useState('');
+  const [selected, setSelected] = useState('');
+
+  useEffect(() => {
+    // imagine this is the value I get directly from the api
+    setLabel(org[0].name);
+  }, [label]);
+
+  console.log('label from api:', label);
+  console.log('label selected:', selected);
   return (
-    <ThemeProvider theme={dark ? darkTheme : lightTheme}>
-      <Wrapper>
-        <Logo variant={dark ? 'PrimaryWhite' : 'Primary'} />
-        <LoginFormCard padding="l">
-          <Heading size="base">Welcome back</Heading>
-          <Text size="s" marginTop="xs">
-            CoachHub is the leading global talent development platform that enables organizations to create a
-            personalized, measurable and scalable coaching program for the entire workforce, regardless of department
-            and seniority level.
-          </Text>
-          <Divider />
-          <Input label="Email" placeholder="nova@coachhub.com" />
-          <PasswordInput label="Password" marginTop="base" placeholder="·······" />
-          <Link href="#" modifier="bold" size="xs" marginTop="xs" alignSelf="start">
-            Forgot your password?
-          </Link>
-          <Button variant="cta" size="l" marginTop="l" squared>
-            Sign in
-          </Button>
-          <Button variant="secondary" size="l" marginTop="base" squared>
-            Sign in with SSO
-          </Button>
-        </LoginFormCard>
-      </Wrapper>
-      <ThemeSwitcher label="Dark mode" checked={dark} onChange={setDark} />
-    </ThemeProvider>
+    <Combobox
+      description="Description"
+      error=""
+      hint="Hint text"
+      label="Label"
+      noResult="No results"
+      onChange={setSelected}
+      placeholder={`doing this to test label: ${label}`}
+      value={label} // I should see this value when the page loads
+    >
+      {org.map((item) => (
+        <Option key={item.id} title={item.name} value={item.name} selected={item.name === label} />
+      ))}
+    </Combobox>
   );
 }
 
